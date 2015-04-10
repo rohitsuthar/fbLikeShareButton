e8e904b2edee5ff14dbfe24a962295570c7a9da4
Rohit Suthar <rohitsuthar@users.noreply.github.com>
2015-04-10 13:52:19 +0530

    fb.php render page

---
+++ views/fb.php
@@ -0,0 +1,14 @@
+<div id="fb-root"></div>
+<script>
+    (function(d, s, id) {
+       var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+            fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+</script>
+<?php $height = ($rendered['width']=='')?'auto':$rendered['width'].'px'; ?>
+
+<div class="fb-like" data-href="<?=$rendered['url'];?>" data-width="<?=$rendered['width']?>" data-height="<?=$rendered['height'];?>" data-layout="<?=$rendered['layout'];?>" data-action="<?=$rendered['action'];?>" data-colorscheme="<?=$rendered['color_scheme'];?>" data-show-faces="<?=$rendered['show_faces'];?>" data-share="<?=$rendered['show_share'];?>" style="display:block; height:<?php echo $height.' !important'; ?>"></div>
+
